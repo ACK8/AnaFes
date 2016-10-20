27e138ce57e098ab3532bd70f5f6a0473f183335
ken60 <kenken60826@gmail.com>
2016-10-20 12:15:49 +0900

    add ghoul

---
+++ Assets/Script/Enemy/EnemySpawn.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject zombie;
+    [SerializeField]
+    private Transform[] spawnPoint;
+    [SerializeField]
+    private int waveNum;
+	[SerializeField]
+	private float spawnWait;
+
+
+    void Start()
+    {
+		StartCoroutine(Spawn(spawnWait));
+    }
+
+    void Update()
+    {
+
+    }
+
+    private IEnumerator Spawn(float second)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(second);
+            Instantiate(zombie, spawnPoint[Random.Range(0, spawnPoint.Length)].transform.position, Quaternion.identity);
+        }
+    }
+}
