a8856a8c4c71a9849781d2a1be7b6221d306cceb
ACKGame <m2127693073@neokidesu.click>
2016-10-20 14:29:44 +0900

    スポーン機能強化

--- Assets/Script/Enemy/EnemySpawn.cs
+++ Assets/Script/Enemy/EnemySpawn.cs
@@ -8,18 +8,37 @@ public class EnemySpawn : MonoBehaviour
     [SerializeField]
     private Transform[] spawnPoint;
     [SerializeField]
-    private int waveNum;
-	[SerializeField]
-	private float spawnWait;
+    private int waveNum = 0;
+    [SerializeField]
+    private float[] waveSwitchingTime;
+    [SerializeField]
+    private float[] spawnWait;
+    [SerializeField]
+    private float spawnWaitOld;
 
 
     void Start()
     {
-		StartCoroutine(Spawn(spawnWait));
+
+        //StartCoroutine(Spawn(spawnWait[waveNum]));
+
+        //spawnWaitOld = spawnWait[waveNum];
     }
 
     void Update()
     {
+        Debug.Log(Time.realtimeSinceStartup);
+
+        if (Time.realtimeSinceStartup > waveSwitchingTime[waveNum])
+        {
+            waveNum += 1;
+        }
+
+        if (Time.realtimeSinceStartup > spawnWait[waveNum] + spawnWaitOld)
+        {
+            Instantiate(zombie, spawnPoint[Random.Range(0, spawnPoint.Length)].transform.position, Quaternion.identity);
+            spawnWaitOld += spawnWait[waveNum];
+        }
 
     }
 
