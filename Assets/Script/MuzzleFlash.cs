2c8d2e3e96606d84e1b6325431c421a3cb0ed580
ken60 <kenken60826@gmail.com>
2016-10-26 10:24:14 +0900

    add muzzleFlash

---
+++ Assets/Script/MuzzleFlash.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MuzzleFlash : MonoBehaviour
+{
+    private Animator anim;
+
+	void Start () 
+	{
+        anim = GetComponent<Animator>();
+        Destroy(gameObject, 0.2f);
+	}
+}
